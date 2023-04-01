cc9888907038b584cf49e63d423b597159a9abde
UHquokka <bananamayong@gmail.com>
2023-04-01 20:40:45 +0900

    map 과 setTods 로 목록에 추가하기

--- src/components/Todos/Todos.jsx
+++ src/components/Todos/Todos.jsx
@@ -2,22 +2,23 @@ import React, { useState } from "react";
 import AddTodo from "../AddTodo/AddTodo";
 
 export default function ToDos() {
-  const [todo, setTodo] = useState({
-    id: 456,
-    text: "밥먹기",
-    status: "active",
-  });
+  const [todos, setTodos] = useState([
+    {
+      id: 456,
+      text: "밥먹기",
+      status: "active",
+    },
+  ]);
   const handleAdd = (todo) => {
     console.log(todo);
-    setTodo({ todo });
+    setTodos([...todos, todo]);
   };
   return (
     <div className="toDosBox">
-      <ul className="toDos">
-        <li key={todo.id}>
-          <input className="checkBox" type="checkbox" />
-          <span className="toDoText">{todo.text}</span>
-        </li>
+      <ul>
+        {todos.map((item) => (
+          <li key={item.id}>{item.text}</li>
+        ))}
       </ul>
 
       <AddTodo onAdd={handleAdd} />
